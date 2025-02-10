9be49186c94b14be3cb765cc101298e4079649b9
Jan Cybulski <13089097+Dzonas@users.noreply.github.com>
2025-02-10 22:43:53 +0100

    tweak test descriptions for exercises 1-2

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,20 +6,20 @@ main :: IO ()
 main = hspec $ do
   describe "NinetyNine" $ do
     describe "myLast" $ do
-      it "myLast returns last element of the list of non-empty list" $ do
+      it "returns last element of a non-empty list" $ do
         myLast [1, 2, 3, 4] `shouldBe` (4 :: Int)
 
-      it "myLast raises an error for empty list" $ do
+      it "raises an error for an empty list" $ do
         myLast [] `shouldThrow` errorCall "empty list"
 
     describe "myButLast" $ do
-      it "myButLast returns second last element of a non-empty list" $ do
+      it "returns second last element of a non-empty list" $ do
         myButLast [1, 2, 3, 4] `shouldBe` (3 :: Int)
 
-      it "myButLast returns an error for an empty list" $ do
+      it "returns an error for an empty list" $ do
         evaluate (myButLast []) `shouldThrow` errorCall "list with less than 2 elements"
 
-      it "myButLast returns an error for a list with one element" $ do
+      it "returns an error for a list with one element" $ do
         evaluate (myButLast [1 :: Int]) `shouldThrow` errorCall "list with less than 2 elements"
 
     describe "elementAt" $ do
